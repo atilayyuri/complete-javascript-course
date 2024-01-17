dba70846af7327cfa9ef31c98f3613b49dca97f9
Atilay Tamkan <attilay.tamkan@basf.com>
2024-01-17 16:34:33 +0100

    learning typeof method

--- 01-Fundamentals-Part-1/starter/scripts.js
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -3,7 +3,9 @@ let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 
-// value is a piece of data, the most fundamental unit of information in programming
+// value is a piece of data, the most fundamental unit of information in programming. Value has a type not variable! Variable simply stores the value which has a type
+
+// variable = value
 
 // variable should always start with lower case
 let firstName = "Jonas";
@@ -13,3 +15,10 @@ console.log(firstName);
 
 //constant variables should be all upper case
 const PI = 3.1415;
+
+let javaBoolean = true;
+console.log(javaBoolean);
+console.log(typeof javaBoolean); // typeof method 
+
+// changing variable (without using let)
+javaBoolean = "YES!";
