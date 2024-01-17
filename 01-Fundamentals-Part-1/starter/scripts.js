b95cc76b2d661d45bdc02ee49ad33e64d41231da
Atilay Tamkan <attilay.tamkan@basf.com>
2024-01-17 15:55:59 +0100

    variable declaration rules

--- 01-Fundamentals-Part-1/starter/scripts.js
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -1,3 +1,15 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is Fun!');
+// if (js === 'amazing') alert('JavaScript is Fun!');
 console.log(40 + 8 + 23 - 10);
+
+
+// value is a piece of data, the most fundamental unit of information in programming
+
+// variable should always start with lower case
+let firstName = "Jonas";
+console.log(firstName);
+
+// the only two symbols that are allowed during variable declaration is _ or $
+
+//constant variables should be all upper case
+const PI = 3.1415;
